d899014b5e9dd06a9a0e147f37876bca415218d4
hglee93 <jeuslhg@gmail.com>
2019-10-25 23:55:28 +0900

    Add VideoLikeRepository.java

---
+++ src/main/java/com/skku/nutube/repository/VideoLikeRepository.java
@@ -0,0 +1,35 @@
+package com.skku.nutube.repository;
+
+import com.skku.nutube.dto.VideoLikeDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class VideoLikeRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    static final String SQL = "select * from likes where userId = ?";
+
+    public List<VideoLikeDto> selectLikes(int userId) {
+        List<VideoLikeDto> videoLikeDtoList = jdbcTemplate.query(SQL, new Object[]{userId}, new VideoLikeDtoMapper());
+        return videoLikeDtoList;
+    }
+
+    public class VideoLikeDtoMapper implements RowMapper<VideoLikeDto> {
+        // interface method
+        public VideoLikeDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+            VideoLikeDto videoLikeDto = new VideoLikeDto();
+            videoLikeDto.setUserId(rs.getInt("userId"));
+            videoLikeDto.setVideoId(rs.getInt("videoId"));
+            videoLikeDto.setLike(rs.getInt("like"));
+            return videoLikeDto;
+        }
+    }
+}
