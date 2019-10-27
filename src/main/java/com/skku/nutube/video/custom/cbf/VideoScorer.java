bf3e201838f2e95f5fed29ca3b0c357ea88a81d7
hglee93 <jeuslhg@gmail.com>
2019-10-27 22:43:11 +0900

    Modify VideoScorer.java
    - Seperate calculate cosine similarity function.

---
+++ src/main/java/com/skku/nutube/video/custom/cbf/VideoScorer.java
@@ -0,0 +1,101 @@
+package com.skku.nutube.video.custom.cbf;
+
+import com.skku.nutube.dto.VideoScoreDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class VideoScorer {
+
+    public Double calCosineSimilarity(Map<String, Double> userProfile, Map<String, Double> iv){
+        // TODO Compute the cosine of this item and the user's profile, store it in the output list
+        // TODO And remove this exception to say you've implemented it
+        Double mulUserItem = 0.0;
+
+        for (Map.Entry<String, Double> e : iv.entrySet()) {
+
+            if (userProfile.containsKey(e.getKey()) == false) {
+                continue;
+            }
+
+            Double userPreference = userProfile.get(e.getKey());
+            mulUserItem += (e.getValue() * userPreference);
+        }
+
+        Double squareUser = 0.0;
+        for(Map.Entry<String, Double> e : userProfile.entrySet()) {
+            squareUser += (e.getValue() * e.getValue());
+        }
+        squareUser = Math.sqrt(squareUser);
+
+        Double squareItem = 0.0;
+        for(Map.Entry<String, Double> e : iv.entrySet()) {
+            squareItem += (e.getValue() * e.getValue());
+        }
+
+        squareItem = Math.sqrt(squareItem);
+
+        // If the denominator of the cosine similarity is 0, skip the item
+        if(squareUser.equals(0.0) || squareItem.equals(0.0)) {
+            return -1.0;
+        }
+
+        Double cs = mulUserItem / (squareItem * squareUser);
+        return cs;
+    }
+
+    public Double calJaccardSimilarity(Map<String, Double> userProfileA, Map<String, Double> userProfileB) {
+        Double intersection = 0.0;
+        Double union = 0.0;
+        Double js = 0.0;
+
+        Map<String, Double> outerMap;
+        Map<String, Double> innerMap;
+
+        if(userProfileA.size() < userProfileB.size()) {
+            outerMap = userProfileA;
+            innerMap = userProfileB;
+        } else {
+            outerMap = userProfileB;
+            innerMap = userProfileA;
+        }
+
+        for(Map.Entry<String, Double> entry : outerMap.entrySet()) {
+            if(innerMap.containsKey(entry.getKey()) == true) {
+                intersection += 1.0;
+            }
+        }
+
+        union = userProfileA.size() + userProfileB.size() - intersection;
+        js = intersection / union;
+
+        return js;
+    }
+
+    public List<VideoScoreDto> scoreWithDetails(Map<String, Double> userProfile,
+                                                Map<Integer, Map<String, Double>> itemVectors,
+                                                Map<Integer, String> itemTitleVectors) {
+
+        List<VideoScoreDto> videoScoreDtoList = new ArrayList<>();
+
+        Set<Integer> items = itemVectors.keySet();
+
+        for (Integer item: items) {
+
+            Map<String, Double> iv = itemVectors.get(item);
+            Double cs = calCosineSimilarity(userProfile, iv);
+
+            if(cs.equals(-1.0)) {
+                continue;
+            }
+
+            videoScoreDtoList.add(new VideoScoreDto(item, itemTitleVectors.get(item), cs));
+        }
+
+        return videoScoreDtoList;
+    }
+}
