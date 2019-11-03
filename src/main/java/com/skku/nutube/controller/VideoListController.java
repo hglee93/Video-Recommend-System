d9b26f2444e51b1d2cebb83d65e9ebbbb2894f30
hglee93 <jeuslhg@gmail.com>
2019-11-03 20:47:22 +0900

    Modify VideoListController.java
    - Remove getTest2List, getTestList

--- src/main/java/com/skku/nutube/controller/VideoListController.java
+++ src/main/java/com/skku/nutube/controller/VideoListController.java
@@ -6,6 +6,7 @@ import com.skku.nutube.dto.VideoLikeDto;
 import com.skku.nutube.dto.VideoScoreDto;
 import com.skku.nutube.repository.VideoLikeRepository;
 import com.skku.nutube.repository.VideoListRepository;
+import com.skku.nutube.repository.VideoRepository;
 import com.skku.nutube.video.custom.cbf.ContentBasedFilter;
 import com.skku.nutube.video.custom.cf.CollaborativeFiltering;
 import com.skku.nutube.video.custom.hybrid.HybridFiltering;
@@ -45,6 +46,9 @@ public class VideoListController {
     VideoListRepository videoListRepository;
 
     @Autowired
+    VideoRepository videoRepository;
+
+    @Autowired
     VideoLikeRepository videoLikeRepository;
 
     @Autowired
@@ -60,13 +64,9 @@ public class VideoListController {
 
     static final int THRESHOLD_CF = 20;
 
-    @RequestMapping(value = "/video/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/video/recommend", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-    public List<VideoScoreDto> getVideoList(@RequestParam(value = "userId", defaultValue = "0")String userId) throws IOException {
-
-        //List<VideoScoreDto> videoScoreDtoList = contentBasedFilter.recommend(Integer.valueOf(userId));
-        //List<VideoScoreDto> videoScoreDtoList = collaborativeFiltering.recommend(Integer.valueOf(userId));
-        //List<VideoScoreDto> videoScoreDtoList = hybridFiltering.recommend(Integer.valueOf(userId));
+    public List<VideoScoreDto> getRecommendVideoList(@RequestParam(value = "userId", defaultValue = "0")String userId) throws IOException {
 
         List<VideoLikeDto> videoLikeDtoList = videoLikeRepository.selectLikesByUserId(Integer.valueOf(userId));
 
@@ -83,119 +83,33 @@ public class VideoListController {
         return videoScoreDtoList;
     }
 
+    @RequestMapping(value = "/video/list", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<VideoScoreDto> getVideoList() throws IOException {
+
+        List<VideoDto> videoList = videoRepository.selectVideo();
+
+        List<VideoScoreDto> videoScoreDtoList = new ArrayList<>();
+
+        Random random = new Random();
+
+        for(int i = 0; i < 20; i++) {
+            int randomId = random.nextInt(videoList.size());
+            VideoScoreDto videoScoreDto = new VideoScoreDto();
+            videoScoreDto.setVideoId(videoList.get(randomId).getVideoId());
+            videoScoreDto.setVideoTitle(videoList.get(randomId).getVideoName());
+            videoScoreDto.setSimilarity(0.0);
+            videoScoreDtoList.add(videoScoreDto);
+        }
+
+        return videoScoreDtoList;
+    }
+
     @RequestMapping(value = "/like", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public void putVideoLike(@RequestParam(value = "userId", defaultValue = "0")String userId,
                              @RequestParam(value = "videoId", defaultValue = "0")String videoId) throws IOException {
         videoLikeRepository.insertLike(Integer.valueOf(userId), Integer.valueOf(videoId));
-    }
-
-    @RequestMapping(value = "/test2/list", method = RequestMethod.GET)
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<VideoDto> getTest2List(@RequestParam(value = "userId", defaultValue = "0")String userId) throws IOException {
-
-        LenskitConfiguration config = null;
-        List<VideoDto> resultList = null;
-
-        try {
-            config = ConfigHelpers.load(new File("etc/basic.groovy"));
-        } catch (IOException e) {
-            throw new RuntimeException("could not load configuration", e);
-        }
-
-        DataAccessObject dao;
-        try {
-            StaticDataSource data = StaticDataSource.load(dataFile);
-            // get the data from the DAO
-            dao = data.get();
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
-
-        LenskitRecommenderEngine engine = LenskitRecommenderEngine.build(config, dao);
-        logger.info("built recommender engine");
-
-        try (LenskitRecommender rec = engine.createRecommender(dao)) {
-            logger.info("obtained recommender from engine");
-            // we want to recommend items
-            ItemRecommender irec = rec.getItemRecommender();
-            assert irec != null;
-
-            // for users
-            ResultList recommendations = irec.recommendWithDetails(Integer.parseInt(userId), 10, null, null);
-            System.out.format("Recommendations for user %d:\n", Integer.parseInt(userId));
-
-            resultList = new ArrayList<>();
-
-            for (Result item : recommendations) {
-                Entity itemData = dao.lookupEntity(CommonTypes.ITEM, item.getId());
-                String name = null;
-                if (itemData != null) {
-                    name = itemData.maybeGet(CommonAttributes.NAME);
-                }
-                //System.out.format("\t%d (%s): %.2f\n", item.getId(), name, item.getScore());
-                //resultList.add(new VideoDto(item.getId(), name, item.getScore()));
-            }
-        }
-
-
-        return resultList;
-    }
-    @RequestMapping(value = "/test/list", method = RequestMethod.GET)
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<VideoDto> getTestList(@RequestParam(value = "userId", defaultValue = "0")String userId) throws IOException {
-
-        LenskitConfiguration config = null;
-        List<VideoDto> resultList = null;
-
-        try {
-            config = ConfigHelpers.load(new File("etc/videobasic.groovy"));
-        } catch (IOException e) {
-            throw new RuntimeException("could not load configuration", e);
-        }
-
-        DataAccessObject dao;
-        try {
-            StaticDataSource data = StaticDataSource.load(videoDataFile);
-            // get the data from the DAO
-            dao = data.get();
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
-
-        LenskitRecommenderEngine engine = LenskitRecommenderEngine.build(config, dao);
-        logger.info("built recommender engine");
-
-        try (LenskitRecommender rec = engine.createRecommender(dao)) {
-            logger.info("obtained recommender from engine");
-            // we want to recommend items
-            ItemRecommender irec = rec.getItemRecommender();
-            assert irec != null; // not null because we configured one
-            //ResultList recommendations = irec.recommend(42, 10);
-            //List<Long> recommendations = irec.recommend(320, 10);
-
-            /*for(Long itemId : recommendations) {
-                System.out.println(itemId);
-            }*/
-            // for users
-            ResultList recommendations = irec.recommendWithDetails(Integer.parseInt(userId), 10, null, null);
-            System.out.format("Recommendations for user %d:\n", Integer.parseInt(userId));
-
-            resultList = new ArrayList<>();
-
-            for (Result item : recommendations) {
-                Entity itemData = dao.lookupEntity(CommonTypes.ITEM, item.getId());
-                String name = null;
-                if (itemData != null) {
-                    name = itemData.maybeGet(CommonAttributes.NAME);
-                }
-                //System.out.format("\t%d (%s): %.2f\n", item.getId(), name, item.getScore());
-                //resultList.add(new VideoDto(item.getId(), name, item.getScore()));
-            }
-        }
-
-
-        return resultList;
     }
 
 }
